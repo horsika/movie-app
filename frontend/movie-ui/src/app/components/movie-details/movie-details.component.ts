6e30f7aa1ca59471ee9ce418d53f7a1945dcd6f7
Orsi <horsika97@gmail.com>
2023-07-27 16:24:24 +0200

    .

---
+++ frontend/movie-ui/src/app/components/movie-details/movie-details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MovieListItem} from "../../models/MovieListItem";
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.css']
+})
+export class MovieDetailsComponent {
+  movie: MovieListItem = {
+    id: 0,
+    title:"sdfgbh",
+    director: "sdfgbnbvcdrtghvc",
+    year: 2020
+  }
+}
